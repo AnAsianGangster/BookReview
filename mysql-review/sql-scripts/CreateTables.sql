5c76f815fc2175bedf359c56704411d79b20baaa
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2020-11-20 21:27:20 +0800

    sql timestamp

--- mysql-review/sql-scripts/CreateTables.sql
+++ mysql-review/sql-scripts/CreateTables.sql
@@ -4,29 +4,38 @@ CREATE TABLE books (
     price FLOAT,
     brand VARCHAR(255),
     imgUrl VARCHAR(255),
-    description VARCHAR(255)
+    description VARCHAR(255),
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL
 );
 
 CREATE TABLE users (
     reviewerID INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255),
-    password VARCHAR(255)
+    password VARCHAR(255),
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL
 );
 
 CREATE TABLE roles (
     id INT PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL
 );
 
 CREATE TABLE userRoles (
     roleId INT REFERENCES roles(id)
         ON DELETE CASCADE,
     reviewerID INT REFERENCES users(reviewerID)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL
 );
 
 CREATE TABLE reviews (
+    id int AUTO_INCREMENT PRIMARY KEY,
     asin VARCHAR(255) REFERENCES books(asin)
         ON DELETE CASCADE,
     helpful FLOAT,
@@ -35,5 +44,7 @@ CREATE TABLE reviews (
     reviewerID INT REFERENCES users(reviewerID)
         ON DELETE CASCADE,
     reviewerName VARCHAR(255),
-    summary VARCHAR(255)
+    summary VARCHAR(255),
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL
 );
