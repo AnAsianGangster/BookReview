428fc0a520a039e9fce48a17eff0fb07ae58980e
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2020-11-20 09:43:31 +0800

    update loggers

--- node-server/app/controllers/book.controller.js
+++ node-server/app/controllers/book.controller.js
@@ -1,6 +1,8 @@
 const db = require('../models/index');
 const Book = db.books;
 
+const logger = require('../config/logger.config');
+
 // create a new book
 exports.createBook = (req, res) => {
     // Validate request
@@ -8,6 +10,7 @@ exports.createBook = (req, res) => {
         res.status(400).send({
             message: 'Content can not be empty!',
         });
+        logger.error('Error empty asin when creating book');
         return;
     }
 
@@ -25,13 +28,13 @@ exports.createBook = (req, res) => {
     Book.create(book)
         .then((data) => {
             res.send(data);
+            logger.info(`Successfully created book: ${book.asin}`);
         })
         .catch((err) => {
             res.status(500).send({
-                message:
-                    err.message ||
-                    'Some error occurred while creating the book.',
+                message: err.message || 'Some error occurred while creating the book.',
             });
+            logger.error(`Error when creating book: ${err.message}`);
         });
 };
 
@@ -40,13 +43,13 @@ exports.findAllBooks = (req, res) => {
     Book.findAll({ include: ['reviews'] })
         .then((data) => {
             res.send(data);
+            logger.info('Successfully find all books');
         })
         .catch((err) => {
             res.status(500).send({
-                message:
-                    err.message ||
-                    'Some error occurred while finding all books with reviews',
+                message: err.message || 'Some error occurred while finding all books with reviews',
             });
+            logger.error(`Error when find all books ${err.message}`);
         });
 };
 
@@ -57,13 +60,13 @@ exports.findBookById = (req, res) => {
     Book.findByPk(asin, { include: ['reviews'] })
         .then((data) => {
             res.send(data);
+            logger.info(`Successfully find book by id: ${asin}`);
         })
         .catch((err) => {
             res.status(500).send({
-                message:
-                    err.message ||
-                    'Some error occurred while finding reviews the book.',
+                message: err.message || 'Some error occurred while finding reviews the book.',
             });
+            logger.err(`Error when find book by id: ${err.message}`);
         });
 };
 
@@ -82,12 +85,12 @@ exports.updateBookById = (req, res) => {
     Book.update(newBook, { where: { asin: asin } })
         .then((result) => {
             res.send(result);
+            logger.info(`Successfully update book: ${asin}`);
         })
         .catch((err) => {
             res.status(500).send({
-                message:
-                    err.message ||
-                    'Some error occurred while updating the book.',
+                message: err.message || 'Some error occurred while updating the book.',
             });
+            logger.error(`Error when updating book: ${err.message}`);
         });
 };
